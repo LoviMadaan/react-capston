14ed091061bc81478a651d1d1ea861a1becdd543
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-05-06 23:07:50 +0530

    components, redux and tests code updated

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { countryDataFetch } from './Redux/country/countrySlice';
+import MyHome from './components/Home/Home';
+import InfoPage from './components/InfoPage/InfoPage';
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(countryDataFetch());
+  });
+
+  return (
+    <Routes>
+      <Route path="/" element={<MyHome />} />
+      <Route path="/country/:name" element={<InfoPage />} />
+    </Routes>
+  );
+}
+
+export default App;
